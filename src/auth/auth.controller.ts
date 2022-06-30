b0b955aea04384744942dcbe77452051f540ce95
Alarm24 <alarmirl24@gmail.com>
2022-07-01 02:17:51 +0700

    Cookie

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
+import {
+  Controller,
+  Request,
+  Post,
+  UseGuards,
+  Response,
+  Options,
+} from '@nestjs/common';
 import { LocalAuthGuard } from './local-auth.guard';
 import { AuthService } from './auth.service';
 import { Public } from 'src/decorator/public.decorator';
@@ -9,8 +16,15 @@ export class AuthController {
 
   @Public()
   @UseGuards(LocalAuthGuard)
-  @Post('login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
+  @Post('/login')
+  async login(@Request() req, @Response() res) {
+    const token = this.authService.login(req.user);
+    res
+      .status(200)
+      .cookie('token', token, {
+        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+      })
+      .json({ message: 'Login successfully' });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser(process.env.COOKIES_SECRET));
   const port = process.env.PORT || 8080;
   await app.listen(port);
 }
